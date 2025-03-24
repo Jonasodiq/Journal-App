63d93d997d52f7922be69ef5a7aa892344b463b2
Jonasodiq <jonasniyazson@hotmail.com>
2025-03-24 11:21:55 +0100

    Added Journal class

---
+++ Journal App/Model/Journal.swift
@@ -0,0 +1,8 @@
+//
+//  Journal.swift
+//  Journal App
+//
+//  Created by Jonas Niyazson on 2025-03-20.
+//
+
+import Foundation
---
+++ Journal App/ViewControllers/ShowEntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShowEntryViewController.swift
+//  Journal App
+//
+//  Created by Jonas Niyazson on 2025-03-20.
+//
+
+import UIKit
+
+class ShowEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
